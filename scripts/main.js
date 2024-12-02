4b0e51c123833012c45cafb3a8c5a34bc72d9c22
paoc456 <paoc456@newschool.edu>
2024-12-01 23:15:24 -0500

    sync i beg

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+document.getElementById('btn1').addEventListener('click', () => {
+    window.location.href = 'entry01.html';
+});
+
+document.getElementById('btn2').addEventListener('click', () => {
+    window.location.href = 'entry02.html';
+});
+
+document.getElementById('btn3').addEventListener('click', () => {
+    window.location.href = 'entry03.html';
+});
+
+document.getElementById('btn4').addEventListener('click', () => {
+    window.location.href = 'entry04.html';
+});
+
+document.getElementById('btn5').addEventListener('click', () => {
+    window.location.href = 'entry05.html';
+});
+
+document.getElementById('btn6').addEventListener('click', () => {
+    window.location.href = 'entry06.html';
+});
+
+document.getElementById('btn7').addEventListener('click', () => {
+    window.location.href = 'entry07.html';
+});
+
+document.getElementById('btn8').addEventListener('click', () => {
+    window.location.href = 'entry08.html';
+});
+
+document.getElementById('btn9').addEventListener('click', () => {
+    window.location.href = 'entry09.html';
+});
+
+document.getElementById('btn10').addEventListener('click', () => {
+    window.location.href = 'entry10.html';
+});
+document.getElementById('btnF').addEventListener('click', () => {
+    window.location.href = 'fullarchive.html';
+});
